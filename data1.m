a5cc6210bcec566f2539ff91bc3e24a298077655
Prasad Tendolkar <pt39@njit.edu>
2014-01-06 08:45:53 -0500

    implement zhang fbcsp (only training)

--- data1.m
+++ data1.m
@@ -1,67 +1,21 @@
 addpath(genpath('../Libraries/eeglab12_0_2_5b'));
-f = ls('dataset1/*calib*.mat');
+f = ls('sets/*.set');
 
 eeglab; clc;
 
 %this for loop iterates over all datasets in dataset1. i've commented out
 %the iteration because we will implement the algorithms for one dataset
 %first.
+
 for fid = 1%:size(f,1) 
    
-   fn = cat(2, 'dataset1/',strtrim(f(fid,:)));
-   data= load(fn);
-    
-   %in order to store eeg information in EEGLAB through scripts, we must
-   %first create a struct with several properties. Some of these properties
-   %are known, but some are not, but they still need to be initialized.
-   NE.setname = strtrim(f(fid,:));
-   NE.filename = '';
-   NE.filepath = '';
-   NE.pnts = size(data.cnt, 1);
-   NE.nbchan = size(data.cnt,2);
-   NE.srate = data.nfo.fs;
-   %NE.ref = '';
-   NE.data = 0.1*double(data.cnt');
-   NE.icawinv = [];
-   NE.icasphere= [];
-   NE.icaweights = [];
-   NE.icaact = [];  
-   NE.trials = 0;
-   NE.comments =[];
-   NE.xmin = 0;
-   NE.xmax = NE.pnts*NE.srate;
-   
-   %channel information processing
-   for i = 1:length(data.nfo.clab)
-        NE.chanlocs(i).labels = data.nfo.clab{i};
-        NE.chanlocs(i).X = data.nfo.xpos(i);
-        NE.chanlocs(i).Y= data.nfo.ypos(i);
-   end
-    
-   %event information processing
-   for i = 1:length(data.mrk.y);
-       NE.event(i).type = data.mrk.y(i);
-       NE.event(i).latency = data.mrk.pos(i);
-   end
-   
-   eeg_checkset(NE);
-   classes=data.nfo.classes;
-   [ALLEEG, ~, ~] = eeg_store(ALLEEG, NE); 
-   
-   %we slice the eeg recordings into several epochs (0.5 to 3.5s after the stimulus), and grab the epochs
-   %with -1 and +1 labels seperately.
-   EEG = pop_epoch( NE, {  '-1'  }, [0.5         3.5], 'newname', '-1 epochs', 'epochinfo', 'yes');
-   [ALLEEG, EEG, ~] = eeg_store(ALLEEG, EEG);
-    class1=EEG.data; %eeg epocjs corresponding to class 1 in a m x n x o (channels x timepoints x epochs) matrix where o is the number of epochs
-   
-   EEG = pop_epoch( NE,  {  '1'  }, [0.5         3.5], 'newname', '+1 epochs', 'epochinfo', 'yes');
-   [ALLEEG, EEG, ~] = eeg_store(ALLEEG, EEG); 
-   class2=EEG.data;
-   eeglab redraw
+   fn = cat(2,strtrim(f(fid,:)));
+   EEG = pop_loadset('filename',fn,'filepath','C:\\Users\\pt2091\\Documents\\MATLAB\\bcicomp\\sets\\');
+   zhang
 end
 %% Zhang's Method
 
 %interval needs 
 
 %%
-classification;
+% classification;
--- zhang.m
+++ zhang.m
@@ -3,37 +3,37 @@
 %%%%%%%%%%%%%%%%%%
 
 fs = EEG.srate;
-fc = [8, 9.75, 11.89, 14.49, 17.67, 21.53, 26.25, 32]; %center frequencies
-qf = 0.33; % q factor (how do you use a q factor with a fc at 32 with qf 0.33?)
-forder = 4;
+
+fc = [8, 9.75, 11.89, 14.49, 17.67, 21.53, 26.25,32]; %center frequencies of each bank
+qf = 1/0.33; % q factor (how do you use a q factor with a fc at 32 with qf 0.33?)
+forder = 4; %filter order
 
 f1 = @(f0,Q) f0*(sqrt(1+1/(4*Q^2))- 1/(2*Q));
 f2 = @(f0,Q) f0*(sqrt(1+1/(4*Q^2))+ 1/(2*Q));
 
-fpi = 1;
-Wn = [ f1(fc(fpi), qf), f2(fc(fpi), qf) ] / (fs/2);
-[b,a] =cheby2(forder,30, Wn); %need to know r value, for now setting to 20
+a = zeros(length(fc),2*forder+1);
+b = zeros(length(fc),2*forder+1);
+
+% get filters coeffs for each bank
+for i = 1:length(fc)
+    Wn = [ f1(fc(i), qf), f2(fc(i), qf) ] / (fs/2);
+    [bi,ai] =cheby2(forder,20, Wn); %need to know r value, for now setting to 20
+    b(i, :) = bi;
+    a(i, :) = ai;
+end
+
 %s = dfilt.df2(b,a);
 %fvtool(s);
 %fres = 100/length(EEG.data(1,:));
 %fa = 0:fres:100-fres;
 %plot(fa,abs(fft(EEG.data(1,:))));
 
-rmpath(genpath('../Libraries/eeglab12_0_2_5b'));
-EEG.data = single(filtfilt(b,a,double(EEG.data')))';
-addpath(genpath('../Libraries/eeglab12_0_2_5b'));
+%% generate class time series
+disp('Generate class time series...');
+lat = [EEG.event.latency];
 
-ALLEEG(1) = EEG;
-eeglab redraw
-%hold on;
-%plot(fa,abs(fft(EEG.data(1,:))), 'r');
-
-%% generate epochs
-windowSize = 1.5; %length of window in seconds
-
-wl = windowSize*fs;
 nEvents = size(EEG.event,2);
-classLabels = zeros(1, EEG.pnts);
+classLabels = zeros(1, EEG.pnts); %class time series
 
 for i = 1:nEvents
     currEvent = EEG.event(i);
@@ -41,6 +41,13 @@ for i = 1:nEvents
     type = currEvent.type;
     classLabels(lat:lat+4*fs) =currEvent.type;
 end
+
+%% generate epochs
+
+windowSize = 1.5; %length of window in seconds
+
+wl = windowSize*fs;
+
 fCL = 1/10; %the fraction of classlabels to use for training 
 nNC = length(find(classLabels(wl:fCL*end) == 0)); %number of NC trials
 nM1 = length(find(classLabels(wl:fCL*end) == 1));
@@ -51,97 +58,71 @@ NC = zeros(59,wl,nNC); M1 = zeros(59, wl,nM1) ; M2 = zeros(59, wl,nM2); %memory 
 nci = 1;
 m1i = 1;
 m2i = 1;
-for i = wl:length(classLabels)*fCL
-    switch classLabels(i)
-        case 0
-            NC(:,:,nci) = EEG.data(:, (i-wl+1):i);
-            nci = nci+1;
-        case 1
-            M1(:,:,m1i) = EEG.data(:, (i-wl+1):i);
-            m1i = m1i+1;
-        case -1
-            M2(:,:,m2i) = EEG.data(:, (i-wl+1):i);
-            m2i = m2i+1;
+
+Wm1vm2 = zeros(4, size(M1, 1), length(fc));
+Wm1vnc1 = zeros(4, size(M1, 1), length(fc));
+Wm1vnc2 = zeros(4, size(M1, 1), length(fc));
+Wm2vnc1 = zeros(4, size(M1, 1), length(fc));
+Wm2vnc2 = zeros(4, size(M1, 1), length(fc));
+
+for fi = 1:length(fc)
+    restoredefaultpath
+%     EEG2.data = filtfilt(b(fi,:), a(fi,:), double(EEG.data'))';
+    addpath(genpath('../Libraries/eeglab12_0_2_5b'));
+    %%Generate epochs
+    fprintf('Beginning CSP of filter bank w/ fc = %0.2f...\n', fc(fi));
+    for i = wl:length(classLabels)*fCL
+            E = EEG.data(:, (i-wl+1):i);
+            E=filtfilt(b(fi,:), a(fi,:), double(E'))';
+            switch classLabels(i)
+                case 0
+                    NC(:,:,nci) = E;
+                    nci = nci+1;
+                case 1
+                    M1(:,:,m1i) = E;
+                    m1i = m1i+1;
+                case -1
+                    M2(:,:,m2i) = E;
+                    m2i = m2i+1;
+            end
     end
-end
-% NC(:,:,1) = [];M1(:,:,1) = [];M2(:,:,1) = [];
 
-%% generate csp vectors
+    %% Finding the PCA substates
+    disp('Finding the PCA substates...');
+    %cp = getNumRelevantPCs(NC) %returns 4.0866 for wl of 1.5s
+    cp = 5;
+    ncu = zeros(cp,size(NC, 3)); %power X epochs
+    for i = 1:size(NC,3)
+        E = NC(:,:,i); 
+        [chans,T]=size(E);
 
-R1 = zeros(size(M1, 1));
-R2 = zeros(size(M1, 1));
-Rnc = zeros(size(M1,1));
-for i = 1:size(M1,3)
-   E = M1(:,:,i);
-   [chans,T]=size(E);
-     
-   E = E-repmat(mean(E,2),[1 T]);
-   
-   tmpC = (E*E');
-   R1 = R1 + tmpC./trace(tmpC);
-end
-R1 = R1/i;
+        E = E-repmat(mean(E,2),[1 T]);
+        [COEFF, SCORE, LATENT] = pca(E');
 
-for i = 1:size(M2,3)
-   E = M2(:,:,i);
-   [chans,T]=size(E);
+        Q = COEFF(:, 1:cp);
 
-   E = E-repmat(mean(E,2),[1 T]);
-   
-   tmpC = (E*E');
-   R2 = R2 + tmpC./trace(tmpC);
-end
-R2 = R2/i;
-
-%just to see if there were any differences....too much overlap too notice
-%anything....
-% for i = 1:size(NC,3)
-%    E = NC(:,:,i);
-%    E = E-repmat(mean(E,2),[1 T]);
-%    
-%    tmpC = (E*E');
-%    Rnc = Rnc + tmpC./trace(tmpC);
-% end
-% Rnc = Rnc/i;
-
-Ccomposite=R1+R2;
-
-[Ucomposite,Lambdacomposite] = eig(Ccomposite);
-[Lambdacomposite,ind] = sort(diag(Lambdacomposite),'descend');
-Ucomposite = Ucomposite(:,ind);
-P=sqrt(inv(diag(Lambdacomposite)))*Ucomposite';
-
-S{1}=P*R1*P';
-S{2}=P*R2*P';
-
-[B,D] = eig(S{1},S{2});
-[D,ind] = sort(diag(D)); B = B(:,ind);
-
-W=(B'*P);
-for i=1:length(ind), W(i,:)=W(i,:)./norm(W(i,:)); end
-%% Finding the PCA substates
-
-%cp = getNumRelevantPCs(NC) %returns 4.0866 for wl of 1.5s
-cp = 5;
-ncu = zeros(cp,size(NC, 3)); %power X epochs
-for i = 1:size(NC,3)
-    E = NC(:,:,i); 
-    [chans,T]=size(E);
+        %zhang equation 6
+        ncu(:,i) = diag((Q'*E)*(Q'*E)');
+    end
     
-    E = E-repmat(mean(E,2),[1 T]);
-    [COEFF, SCORE, LATENT] = pca(E');
+    disp('Running k-means...');
+    k = 2; %the paper says 2 clusters are the best, we will cv to make sure later on
+    [IDX, C] = kmeans(ncu', 2);
+    NCS1 = NC(:,:, IDX==1);
+    NCS2 = NC(:,:, IDX==2);
 
-    Q = COEFF(:, 1:cp);
+    %% generate csp vectors
+    disp('Generate csp vectors...');
+
+    Wm1vm2(:,:,fi) = getCSPVectors(M1, M2);
+    Wm1vnc1(:,:,fi) = getCSPVectors(M1, NCS1);
+    Wm1vnc2(:,:,fi) = getCSPVectors(M1, NCS2);
+    Wm2vnc1(:,:,fi) = getCSPVectors(M2, NCS1);
+    Wm2vnc2(:,:,fi) = getCSPVectors(M2, NCS2);
     
-    %zhang equation 6
-    ncu(:,i) = diag((Q'*E)*(Q'*E)');
-    disp(i);
+    %% grab features
+    
 end
-k = 2; %the paper says 2 clusters are the best, we will cv to make sure later on
-[IDX, C] = kmeans(ncu', 2);
-NCS1 = NC(:,:, IDX==1);
-NCS2 = NC(:,:, IDX==2);
-
 %% feature selection
 
 
